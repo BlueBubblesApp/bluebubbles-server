9936571fa6f92b68ce52cd69653513556a605695
Zach Shames <zach.shames@threatq.com>
2020-06-25 16:45:41 -0400

    Upgrades electron

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 // core components
 import Admin from "./layouts/Admin";
