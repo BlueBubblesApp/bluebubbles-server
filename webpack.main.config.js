d8dcbeaae69335b378b3bde4b4820f2ec330775f
zlshames <zlshames@gmail.com>
2021-12-13 10:53:46 -0500

    fixes build issues

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -32,6 +32,14 @@ module.exports = merge.smart(baseConfig, {
                         [
                             "@babel/plugin-proposal-class-properties",
                             { loose: true }
+                        ],
+                        [
+                            "@babel/plugin-proposal-private-methods",
+                            { loose: true }
+                        ],
+                        [
+                            "@babel/plugin-proposal-private-property-in-object",
+                            { loose: true }
                         ]
                     ]
                 }
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -28,7 +28,18 @@ module.exports = merge.smart(baseConfig, {
                         '@babel/preset-react'
                     ],
                     plugins: [
-                        ['@babel/plugin-proposal-class-properties', { loose: true }]
+                        [
+                            "@babel/plugin-proposal-class-properties",
+                            { loose: true }
+                        ],
+                        [
+                            "@babel/plugin-proposal-private-methods",
+                            { loose: true }
+                        ],
+                        [
+                            "@babel/plugin-proposal-private-property-in-object",
+                            { loose: true }
+                        ]
                     ]
                 }
             },
