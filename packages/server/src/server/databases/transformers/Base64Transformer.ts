7dd7f0807c437ef66acc020fd454ebb73d411e36
zlshames <zlshames@gmail.com>
2022-04-10 17:31:04 -0400

    Fixes transformer issue

--- packages/server/src/server/databases/transformers/Base64Transformer.ts
+++ packages/server/src/server/databases/transformers/Base64Transformer.ts
@@ -1,6 +1,6 @@
 import { ValueTransformer } from "typeorm";
 
 export const Base64Transformer: ValueTransformer = {
-    from: dbValue => Buffer.from(dbValue, 'base64'),
-    to: entityValue => entityValue.toString('base64')
+    from: dbValue => dbValue ? Buffer.from(dbValue, 'base64') : null,
+    to: entityValue => entityValue ? entityValue.toString('base64') : null
 };
