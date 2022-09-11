2031317596b7d0ae9efa322939740003cdf0a36f
zlshames <zlshames@gmail.com>
2022-09-11 10:49:51 -0400

    fix: backward compat typed-stream

--- packages/server/src/server/databases/transformers/AttributedBodyTransformer.ts
+++ packages/server/src/server/databases/transformers/AttributedBodyTransformer.ts
@@ -1,18 +1,32 @@
 import { ValueTransformer } from "typeorm";
 import {NSAttributedString, Unarchiver} from "node-typedstream";
 import {Server} from "@server";
+import { isEmpty } from "@server/helpers/utils";
 
 export const AttributedBodyTransformer: ValueTransformer = {
     from: dbValue => {
         try {
-            const attributedBody = Unarchiver.open(dbValue).decodeAll()[0].values[0];
-            if (attributedBody instanceof NSAttributedString) {
-                return attributedBody;
-            }
+            const attributedBody = Unarchiver.open(dbValue).decodeAll();
+            if (isEmpty(attributedBody)) return null;
+
+            const attributedBodies = attributedBody[0].values.filter((e) => {
+                return e && e instanceof NSAttributedString;
+            }).map((e) => {
+                // Rename value to string for backwards compatibility
+                if (Object.keys(e).includes("value")) {
+                    e.string = e.value;
+                    delete e.value;
+                }
+
+                return e;
+            });
+
+            return attributedBodies;
         } catch (e: any) {
             Server().log(`Failed to deserialize attributedBody: ${e.message}`, "debug");
         }
+
         return null;
     },
-    to: entityValue => null
+    to: _ => null
 };
