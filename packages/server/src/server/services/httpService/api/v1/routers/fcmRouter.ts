d8365a82aa2789c997c970b4d4d2fe92600e2ec4
zlshames <zlshames@gmail.com>
2023-05-27 13:48:56 -0400

    fix: monkeypatch client_id into google services

--- packages/server/src/server/services/httpService/api/v1/routers/fcmRouter.ts
+++ packages/server/src/server/services/httpService/api/v1/routers/fcmRouter.ts
@@ -4,10 +4,28 @@ import { RouterContext } from "koa-router";
 import { FileSystem } from "@server/fileSystem";
 import { GeneralInterface } from "@server/api/v1/interfaces/generalInterface";
 import { Success } from "../responses/success";
+import { isEmpty } from "@server/helpers/utils";
 
 export class FcmRouter {
     static async getClientConfig(ctx: RouterContext, _: Next) {
-        return new Success(ctx, { data: FileSystem.getFCMClient() }).send();
+        const googleServices = FileSystem.getFCMClient();
+
+        // As of May 2023, Google removed the `oauth_client[]` data from the `google-services.json` file.
+        // As such, we need to manually add it back in so the client doesn't break when reading the file.
+        // This is a monkeypatch. The client only requires the first part of the client_id, before the `-`.
+        // This value is the same as the project number, so we can just use that.
+        if (isEmpty(googleServices.oauth_client)) {
+            googleServices.oauth_client = [
+                {
+                    // The project number exists in multiple fields. Fallback if the project number is not found.
+                    client_id: googleServices?.project_info?.project_number ??
+                        googleServices?.client[0].client_info.mobilesdk_app_id.split(":")[1],
+                    client_type: 3
+                }
+            ];
+        }
+
+        return new Success(ctx, { data: googleServices }).send();
     }
 
     static async registerDevice(ctx: RouterContext, _: Next) {
