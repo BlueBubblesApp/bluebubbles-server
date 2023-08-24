ee5613f4456a7db4b62df51be1c3dd13d12a1ae7
zlshames <zlshames@gmail.com>
2023-08-24 15:53:16 -0400

    fix: dylib issue pre-catalina

--- packages/server/src/server/services/privateApi/modes/ProcessDylibMode.ts
+++ packages/server/src/server/services/privateApi/modes/ProcessDylibMode.ts
@@ -1,4 +1,5 @@
 import "zx/globals";
+import fs from "fs";
 import { isMinBigSur, isMinMonterey, waitMs } from "@server/helpers/utils";
 import { PrivateApiMode } from ".";
 import { Server } from "@server";
@@ -22,15 +23,29 @@ export class ProcessDylibMode extends PrivateApiMode {
         return path.join(FileSystem.resources, "private-api", macVer, "BlueBubblesHelper.dylib");
     }
 
-    static messagesPath = "/System/Applications/Messages.app/Contents/MacOS/Messages";
+    static get messagesPath() {
+        // Paths are different on Pre/Post Catalina.
+        // We are gonna test for both just in case an app was installed prior to the OS upgrade.
+        const possiblePaths = [
+            "/System/Applications/Messages.app/Contents/MacOS/Messages",
+            "/Applications/Messages.app/Contents/MacOS/Messages"
+        ];
+
+        // Return the first path that exists
+        for (const path of possiblePaths) {
+            const exists = fs.existsSync(path);
+            if (exists) return path;
+        }
+
+        return null;
+    }
     
     static async install() {
         if (!fs.existsSync(ProcessDylibMode.dylbiPath)) {
             throw new Error("Unable to locate embedded Private API DYLIB! Please reinstall the app.");
         }
 
-        const messagesPath = "/System/Applications/Messages.app/Contents/MacOS/Messages";
-        if (!fs.existsSync(messagesPath)) {
+        if (!this.messagesPath) {
             throw new Error("Unable to locate Messages.app! Please give the BlueBubbles Server Full Disk Access.");
         }
 
@@ -47,6 +62,8 @@ export class ProcessDylibMode extends PrivateApiMode {
     }
 
     async manageProcess() {
+        const messagesPath = ProcessDylibMode.messagesPath;
+
         // Clear the markers
         this.dylibFailureCounter = 0;
         this.dylibLastErrorTime = 0;
@@ -65,7 +82,7 @@ export class ProcessDylibMode extends PrivateApiMode {
 
                 // Execute shell command to start the dylib.
                 // eslint-disable-next-line max-len
-                this.dylibProcess = $`DYLD_INSERT_LIBRARIES=${ProcessDylibMode.dylbiPath} ${ProcessDylibMode.messagesPath}`;
+                this.dylibProcess = $`DYLD_INSERT_LIBRARIES=${ProcessDylibMode.dylbiPath} ${messagesPath}`;
                 await this.dylibProcess;
             } catch (ex: any) {
                 if (this.isStopping) return;
