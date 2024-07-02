64139b22ba2a35ce635db31a456d22df0115d6a5
zlshames <zlshames@gmail.com>
2024-07-02 11:58:10 -0400

    adds back arm64 builds

--- packages/server/scripts/electron-builder-config.js
+++ packages/server/scripts/electron-builder-config.js
@@ -39,7 +39,7 @@ module.exports = {
                 "target": "dmg",
                 "arch": [
                     "x64",
-                    // "arm64"
+                    "arm64"
                 ]
             }
         ],
